d2d7931cf93b0b685e18b45d8eeb4db8c5e77c33
Gustav Holm <gustav-h@live.se>
2023-10-19 14:25:33 +0200

    ändringar på league och sidebar

--- src/app/league/league.component.ts
+++ src/app/league/league.component.ts
@@ -40,6 +40,7 @@ export class LeagueComponent implements OnInit {
   // bookingLeaders: any[] = [];
   // sentOffLeaders: any[] = [];
 
+  colorDescription: string = '';
   receivedData: string = '';
   selectedSeason: any;
   leagueName: string = '';
@@ -66,6 +67,7 @@ export class LeagueComponent implements OnInit {
     this.seasons = [];
     this.leagueService.getLeagues().subscribe((response) => {
       this.leagues = response.leagues;
+
       this.getSeasonsBasedOnLeague();
     });
   }
@@ -78,6 +80,7 @@ export class LeagueComponent implements OnInit {
           this.seasons = response.leagues;
           this.getLeagueStats(this.seasons[0].id);
           this.leagueName = this.seasons[0].name;
+
           if (this.seasons.length > 0) {
             this.seasonControl.setValue(this.seasons[0]);
           }
@@ -123,6 +126,82 @@ export class LeagueComponent implements OnInit {
     // this.leagueService.getSentOffLeaders(seasonId).subscribe((response) => { //Sets the sent off leaders
     //   this.sentOffLeaders = response.groups[0].standings;
     // });
+  }
+
+  getColor(index: number) {
+    if (this.leagueName.toLowerCase() === 'svenska hockeyligan shl') {
+      this.colorDescription = 'Green = Playoff, Yellow = Play-ins, Red = Relegation Series';
+      if (index <= 6) {
+        return 'green';
+      } else if (index >= 7 && index <= 10) {
+        return 'yellow';
+      } else if (index >= 13 && index <= 14) {
+        return 'red';
+      }
+    } else if (this.leagueName.toLowerCase() === 'allsvenskan') {
+      this.colorDescription = 'Blue = Champions league Qualifier, Orange = Europa Conference League Qualifier, Yellow = Relegation Qualifier, Red = Relegated'
+      if (index == 1) {
+        return 'blue';
+      } else if (index >= 2 && index <= 3) { 
+        return '#cc8006';
+      } else if (index == 14) { 
+        return 'yellow';
+      } else if (index >= 15 && index <= 16) { 
+        return 'red';
+      } 
+    } else if (this.leagueName.toLowerCase() === 'damallsvenskan') {
+      this.colorDescription = 'Blue = Champions League Qualifier, Yellow = Relegation Qualifier, Red = Relegated'
+      if (index >= 1 && index <= 3) {
+        return 'blue';
+      } else if (index == 12) { 
+        return 'yellow';
+      } else if (index >= 13 && index <= 14) { 
+        return 'red';
+      }
+    } else if (this.leagueName.toLowerCase() === 'superettan') {
+      this.colorDescription = 'Blue = Promoted, Orange = Promotion Qualifier, Yellow = Relegation Qualifier, Red = Relegated'
+      if (index >= 1 && index <= 2) {
+        return 'blue';
+      } else if (index == 3) {
+        return '#cc8006';
+      } else if (index >= 13 && index <= 14) {
+        return 'yellow';
+      } else if (index >= 15 && index <= 16) {
+        return 'red';
+      }
+    } else if (this.leagueName.toLowerCase() === 'sdhl') {
+      this.colorDescription = 'Green = Playoffs, Red = Relegation Qualifiers';
+      if (index >= 1 && index <= 8) {
+        return 'green';
+      } else if (index >= 9) {
+        return 'red';
+      }
+    } else if (this.leagueName.toLowerCase() === 'superligan ssl') {
+      this.colorDescription = 'Green = Playoffs, Red = Relegated'
+      if (index >= 1 && index <= 8) {
+        return 'green';
+      } else if (index >= 13 && index <= 14) {
+        return 'red';
+      }
+    }
+
+    return '';
+
+    //getIceHockeyRowColor(): string {
+    // Add your color logic here for the 'Ishockey' sport
+    // Define your color pattern based on the row index
+    // Example:
+    // if (i >= 0 && i <= 5) {
+    //   return 'green';
+    // } else if (i >= 6 && i <= 9) {
+    //   return 'blue';
+    // } else if (i >= 10 && i <= 11) {
+    //   return 'gray';
+    // } else if (i >= 12 && i <= 13) {
+    //   return 'red';
+    // }
+    //return ''; // Return the appropriate color class based on your color pattern
+    //}
   }
 
 
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -9,7 +9,7 @@ import { ActivatedRoute, RouterLink, Router } from '@angular/router';
   selector: 'app-sidebar',
   standalone: true,
   imports: [CommonModule,
-    RouterLink, RouterLink
+    RouterLink
   ],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
@@ -21,35 +21,34 @@ export class SidebarComponent implements OnInit {
   receivedData: string = '';
   uniqueLeagueNames: Set<string> = new Set<string>();
   toggleSidebarContent: boolean = true;
+  selectedItem: string | null = null; // Variable to track selected item
 
 
   constructor(private route: ActivatedRoute, private router: Router, private sportService: SportService, private leagueService: LeagueService) { }
 
   ngOnInit() {
-   
-    this.route.params.subscribe(params => {
-      let newReceivedData='';
-      if('sport' in params){
-         newReceivedData = params['sport'].charAt(0).toUpperCase() + params['sport'].slice(1);
-       
-      }
-    
-      
-      if ('league' in params) {
-        this.toggleSidebarContent=false;
-      }
-      if (this.receivedData !== newReceivedData) {
-        this.receivedData = newReceivedData;
-        this.getDataBasedOnMessage();
-      }
-    });
+
     this.sportService.getSports().subscribe((response) => {
       this.sports = response.sports;
     });
+
     this.leagueService.getLeagues().subscribe((response) => {
       this.leagues = response.leagues;
-      this.filterUniqueLeagueNames();
-    })
+    });
+
+    this.route.params.subscribe(params => {
+      let newReceivedData = '';
+      if ('sport' in params) {
+        newReceivedData = params['sport'].charAt(0).toUpperCase() + params['sport'].slice(1);
+      }
+
+      if ('league' in params) {
+        this.toggleSidebarContent = false;
+        this.selectedItem = params['league'];
+      }
+      this.receivedData = newReceivedData;
+      this.getDataBasedOnMessage();
+    });
   }
 
   private getDataBasedOnMessage() {
@@ -63,18 +62,18 @@ export class SidebarComponent implements OnInit {
 
   private filterUniqueLeagueNames() {
     this.leagues.forEach((league) => {
-      const name = league.name;
-      if (this.receivedData === "") {
-        this.uniqueLeagueNames.add(league.name.toLowerCase());
-      }
-      if (!this.uniqueLeagueNames.has(name)) {
-
-        if (league.sport.name === this.receivedData) {
+      if (!this.uniqueLeagueNames.has(league.name)) {
+        if (this.receivedData === "") {
+          this.uniqueLeagueNames.add(league.name.toLowerCase());
+        } else if (league.sport.name === this.receivedData) {
           this.uniqueLeagueNames.add(league.name.toLowerCase());
         }
-
       }
     });
+  }
+
+  toggleSubMenu(leagueName: string) {
+    this.selectedItem = this.selectedItem === leagueName ? null : leagueName;
   }
 
   transformWord(word: string) {
@@ -82,20 +81,12 @@ export class SidebarComponent implements OnInit {
   }
 
   createLink(checkSport: string) {
-
     const foundLeague = this.leagues.find(league => league.name.toLowerCase() === checkSport.toLowerCase());
     if (foundLeague) {
-      let link;
-      if (this.receivedData !== "") {
-        link = checkSport;
-
-      } else {
-        link = foundLeague.sport.slug + "/" + checkSport;
-      }
-
-      return link;
+      return foundLeague.sport.slug;
     }
     return null;
   }
+  
 
 }
