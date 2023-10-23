a160d8cb4384f686fb715cd5600cfb091f9cefae
Pehr Torgeir <Pehr.torgeir@hotmail.com>
2023-10-23 17:42:04 +0200

    påbörjat fix för hemskärmarna

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EventService } from '../event.service';
 import { CommonModule } from '@angular/common';
+import { SportService } from '../sport.service';
+
 
 
 @Component({
@@ -15,16 +17,61 @@ export class HomeComponent implements OnInit {
   recentMatchesAmount: number = 3;
   featuredLeague: bigint = 87302n;
   featuredEvents: any[] = [];
-
-  constructor(private router: ActivatedRoute, private eventService: EventService) {}
+  newReceivedData: string = '';
+  sportsArray: any[] = [];
+  featuredSport: bigint = 0n;
+  constructor(private route: ActivatedRoute, private eventService: EventService, private sportService: SportService) { }
 
   ngOnInit(): void {
+
+    this.route.params.subscribe(params => {
+      if ('sport' in params) {
+        this.newReceivedData = params['sport'];
+      }
+
+    });
     this.importFeaturedEvents();
   }
 
+
+
+
+
   private importFeaturedEvents() {
-    this.eventService.getRecentEvents(10).subscribe((response) => {
-      this.featuredEvents = response.events;
-    });
+
+
+    if (this.newReceivedData === '') {
+
+      this.eventService.getRecentEvents(10).subscribe((response) => {
+        this.featuredEvents = response.events;
+      });
+    } else {
+
+      this.sportService.getSports().subscribe((response) => {
+        this.sportsArray = response.sports;
+
+
+        this.sportsArray.forEach(element => {
+          console.log(this.newReceivedData);
+
+          if (element.slug === this.newReceivedData) {
+            this.featuredSport = element.id;
+            console.log(this.featuredSport);
+
+
+
+          }
+        });
+
+
+      });
+      console.log(this.featuredSport);
+
+
+
+
+
+      this.eventService.getRecentEventsForSport(this.featuredSport);
+    }
   }
 }
