872d6746d35bfa770aef2cf25ace69e877673979
Gustav Holm <gustav-h@live.se>
2023-10-31 14:30:28 +0100

    mer tester för att lyckas med deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-    apps : [{
-      name: 'angular-app',
-      script: 'node_modules/@angular/cli/bin/ng',
-      args: 'serve --host 0.0.0.0 --port 4200',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'production'
-      }
+    apps: [{
+        name: 'agilaangularna-1',
+        script: 'node_modules/@angular/cli/bin/ng',
+        args: 'serve --host 0.0.0.0 --port 4200',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        env: {
+            NODE_ENV: 'production'
+        }
     }]
-  };
-  
+};
