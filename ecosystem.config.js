adc8b931eef62db78f71052fe9407088d504a0c3
Gustav Holm <gustav-h@live.se>
2023-10-31 14:45:55 +0100

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [{
       name: 'agilaangularna-1',
-      script: '/home/jelastic/ROOT/node_modules/@angular/cli/bin/ng',
+      script: '/home/jelastic/ROOT/node_modules/@angular/cli/bin/ng.js',
       args: 'serve --host 0.0.0.0 --port 4200',
       instances: 1,
       autorestart: true,
